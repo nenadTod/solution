9bfcc32cfda2ba5ee0ce9ef56719864aa328e468
Amela Špica <amela.spica@gmail.com>
2016-10-15 22:02:19 +0200

    patnja

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var prompts = require('./prompts');
 //proba
 
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function(){console.log('im listening');});
+server.listen(process.env.port || process.env.PORT || 3000, function(){console.log('im listening on port ' + process.env.PORT);});
+
 var connector = new builder.ChatConnector({
     appId : process.env.MICROSOFT_APP_ID,
     appPassword : process.env.MICROSOFT_APP_PASSWORD});
