a6cb810de8b4193b7691b594a139733b363f8d99
Amela Špica <amela.spica@gmail.com>
2016-10-15 22:57:23 +0200

    nesto

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var bot = new builder.UniversalBot(connector);
 server.post('api/solution', connector.listen());
 
 server.get('api/solution', function (req, res, next) {
-  console.log(req.query);
+  console.log("ispis" + req.query);
   //if (req.query['hub.verify_token'] == 'testbot_verify_token') {
     res.send(req.query['hub.challenge']);
   /*} else {
