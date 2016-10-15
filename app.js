1fb7081e2485ec1c3e4a9600e679e83342f6d968
Amela Špica <amela.spica@gmail.com>
2016-10-15 23:02:27 +0200

    nesto

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ server.listen(process.env.port || process.env.PORT || 3978, function(){
   console.log('im listening on port ' + process.env.PORT + " bla: " + server.name + " ijf " + server.url);
 });
 
+
+server.use(restify.queryParser());
+
 var connector = new builder.ChatConnector({
     appId : process.env.MICROSOFT_APP_ID,
     appPassword : process.env.MICROSOFT_APP_PASSWORD});
