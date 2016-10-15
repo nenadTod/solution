b922bbec3a1c636996bc3d40d5cc2ef164fbeac1
Amela Špica <amela.spica@gmail.com>
2016-10-15 22:12:36 +0200

    ludilo

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var prompts = require('./prompts');
 //proba
 
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 80, function(){console.log('im listening on port ' + process.env.PORT);});
-console.log(server.listen());
+server.listen(process.env.port || process.env.PORT || 80, function(){console.log('im listening on port ' + process.env.PORT + " bla: " + server.name + " ijf " + server.url);});
+
 var connector = new builder.ChatConnector({
     appId : process.env.MICROSOFT_APP_ID,
     appPassword : process.env.MICROSOFT_APP_PASSWORD});
