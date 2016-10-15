82968f9f6415c1fa318f918a8483e43f7d347fef
Amela Špica <amela.spica@gmail.com>
2016-10-15 22:50:08 +0200

    nesto

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('api/solution', connector.listen());
 
-server.get('/api/solution', function (req, res, next) {
-  console.log(req.query['hub.verify_token']);
+server.get('api/solution', function (req, res, next) {
+  console.log(req);
   if (req.query['hub.verify_token'] == 'testbot_verify_token') {
     res.send(req.query['hub.challenge']);
   } else {
